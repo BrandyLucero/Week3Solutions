d1d8dbd454672f9a4f2ddc5ab6708394317bb172
Brandy C. Lucero <brandy.c.lucero@gmail.com>
2014-09-09 01:53:24 -0600

    9/8

--- DigitalRootBL/DigitalRootBL/Program.cs
+++ DigitalRootBL/DigitalRootBL/Program.cs
@@ -30,24 +30,30 @@ namespace DigitalRootBL
             bool isANumber = long.TryParse(rootThis, out number);
 
             Console.WriteLine();
-
+            //if statement for rootThis
             if (isANumber == true && rootThis.Length < 100 && !rootThis.Contains(".") && !rootThis.Contains("-"))
             {
+                //declare long total variable
                 long total = 0;
-
+                //for loop
                 for (int i = 0; i < rootThis.Length; i++)
                 {
+                    //declare long number2 variable
                     long number2 = long.Parse(rootThis[i].ToString());
+                    //declare total variable
                     total += number;
                 }
+                //While loop for total
                 while (total.ToString().Length > 1)
                 {
                     for (int i = 0; i < total.ToString().Length; i++)
                     {
+                        //lambda expression for total
                         total = long.Parse(total.ToString()[i].ToString());
                     }
 
                 }
+                //Outputs for RootThis
                 Console.WriteLine("Input: " + rootThis);
                 Console.WriteLine("Output: " + total);
                 Console.WriteLine();
